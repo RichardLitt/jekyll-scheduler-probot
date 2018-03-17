c5bbdb78eb2969a9594fecc71cd3789de1b2bf49
Richard Littauer <richard.littauer@gmail.com>
2018-03-17 17:41:34 -0400

    Small fixes

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const probot = require('probot')
 const createScheduler = require('probot-scheduler');
 
 module.exports = robot => {
-  // Once every 30 seconds for testing
-  const interval = 30*1000 // 60*60*1000 is default, and is for an hour
+  // Once every 3 seconds for testing
+  const interval = 3 * 1000 // 60*60*1000 is default, and is for an hour
 
   scheduler = createScheduler(robot, {'interval': interval});
 
   robot.on('schedule.repository', context => {
     // this event is triggered on an interval, which is 1 hr by default;
-    return context.github.requestPageBuild({
+    return context.github.repos.requestPageBuild({
       owner: 'RichardLitt',
       repo: '2117.91USD',
       // Shouldn't need these, as they're in https://github.com/octokit/rest.js/blob/master/lib/routes.json#L8562
-      // headers: {
-      //   accept: 'application/vnd.github.mister-fantastic-preview+json'
-      // }
+      headers: {
+        accept: 'application/vnd.github.mister-fantastic-preview+json'
+      }
     })
   })
 
