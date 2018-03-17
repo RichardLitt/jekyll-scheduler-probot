ddc1bbbe6b54531a4387ff2b6ce5aeb8580063dd
Richard Littauer <richard.littauer@gmail.com>
2018-03-17 16:50:45 -0400

    Initital commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const probot = require('probot')
+const createScheduler = require('probot-scheduler');
+
+module.exports = robot => {
+  robot.log('Started')
+  // Twice a minute for testing
+  // const interval = 30*1000 // 60*60*1000 is default, and is for an hour
+  //
+  // scheduler = createScheduler(robot, {'interval': interval});
+  //
+  // robot.on('schedule.repository', context => {
+  //   // this event is triggered on an interval, which is 1 hr by default;
+  //   return context.github.requestPageBuild({
+  //     owner: 'RichardLitt',
+  //     repo: '2117.91USD',
+  //     // Shouldn't need these, as they're in https://github.com/octokit/rest.js/blob/master/lib/routes.json#L8562
+  //     // headers: {
+  //     //   accept: 'application/vnd.github.mister-fantastic-preview+json'
+  //     // }
+  //   })
+  // });
+
+}
